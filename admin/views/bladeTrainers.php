e5be577de2a27dd9f03e909ab0eac8205b6744d8
MRxwr <nasserhatab@gmail.com>
2024-08-03 01:10:45 +0300

    Update bladeTrainers.php

--- admin/views/bladeTrainers.php
+++ admin/views/bladeTrainers.php
@@ -97,7 +97,7 @@ if( isset($_POST["fullName"]) ){
 			</select>
 			</div>
 			
-			<div class="col-md-6" style="margin-top:10px">
+			<div class="col-md-12" style="margin-top:10px">
 			<input type="submit" class="btn btn-primary" value="<?php echo direction("Submit","أرسل") ?>">
 			<input type="hidden" name="update" value="0">
 			</div>
