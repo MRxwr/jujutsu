f0170468d99f8004712a7fff5bd7d9bfc2e4a240
MRxwr <nasserhatab@gmail.com>
2024-08-03 11:11:48 +0300

    save

---
+++ admin/views/baldeCountries.php
@@ -0,0 +1,71 @@
+<?php 
+if( isset($_GET["hide"]) && !empty($_GET["hide"]) ){
+	if( updateDB('belts',array('hidden'=> '2'),"`id` = '{$_GET["hide"]}'") ){
+		header("LOCATION: ?v=Countries");
+	}
+}
+
+if( isset($_GET["show"]) && !empty($_GET["show"]) ){
+	if( updateDB('belts',array('hidden'=> '0'),"`id` = '{$_GET["show"]}'") ){
+		header("LOCATION: ?v=Countries");
+	}
+}
+?>		
+				<!-- Bordered Table -->
+<div class="col-sm-12">
+<div class="panel panel-default card-view">
+<div class="panel-heading">
+<div class="pull-left">
+<h6 class="panel-title txt-dark"><?php echo direction("List of Countries","قائمة البلدان") ?></h6>
+</div>
+<div class="clearfix"></div>
+</div>
+<div class="panel-wrapper collapse in">
+<div class="panel-body"> 
+<div class="table-wrap mt-40">
+<div class="table-responsive">
+	<table class="table display responsive product-overview mb-30" id="myTable">
+		<thead>
+		<tr>
+		<th>#</th>
+		<th><?php echo direction("Country","البلد") ?></th>
+		<th class="text-nowrap"><?php echo direction("Actions","الخيارات") ?></th>
+		</tr>
+		</thead>
+		
+		<tbody>
+		<?php 
+		if( $cities = selectDB('cities',"`id` != '0' GROUP BY `CountryName` ORDER BY `CountryName` ASC") ){
+			for( $i = 0; $i < sizeof($cities); $i++ ){
+				$counter = $i + 1;
+				if ( $cities[$i]["hidden"] == 2 ){
+					$icon = "fa fa-eye";
+					$link = "?v={$_GET["v"]}&show={$cities[$i]["id"]}";
+					$hide = direction("Show","إظهار");
+				}else{
+					$icon = "fa fa-eye-slash";
+					$link = "?v={$_GET["v"]}&hide={$cities[$i]["id"]}";
+					$hide = direction("Hide","إخفاء");
+				}
+				?>
+				<tr>
+				<td><?php echo $cities[$i]["CountryName"] ?></td>
+				<td class="text-nowrap">
+					<a href="<?php echo $link ?>" class="mr-25" data-toggle="tooltip" data-original-title="<?php echo $hide ?>"> <i class="<?php echo $icon ?> text-inverse m-r-10"></i>
+					</a>
+				</td>
+				</tr>
+				<?php
+			}
+		}
+		?>
+		</tbody>
+		
+	</table>
+</div>
+</div>
+</div>
+</div>
+</div>
+</div>
+</div>
