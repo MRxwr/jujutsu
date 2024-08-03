7ab57fe8f44c043fcf77f09709a0427c5f368222
MRxwr <nasserhatab@gmail.com>
2024-08-04 00:28:25 +0300

    Update responsive-datatable-data.js

--- admin/dist/js/responsive-datatable-data.js
+++ admin/dist/js/responsive-datatable-data.js
@@ -6,6 +6,9 @@ $(document).ready(function() {
 	$('#myTable').DataTable( {
 		responsive: true
 	} );
+} );
+
+$(document).ready(function() {
 	$('#myTable1').DataTable( {
 		responsive: true
 	} );
