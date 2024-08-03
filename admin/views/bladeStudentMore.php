738e5ff14677afdd5798568df1086063d671ebcb
MRxwr <nasserhatab@gmail.com>
2024-08-04 00:23:18 +0300

    save

--- admin/views/bladeStudentMore.php
+++ admin/views/bladeStudentMore.php
@@ -46,9 +46,9 @@ if( isset($_GET["id"]) && !empty($_GET["id"]) && $student = selectDBNew("student
 			    <label><?php echo direction("Session","الكلاس") ?></label>
 				<select name="sessionId" class="form-control">
 					<?php
-						if( $sessions = selectDB('sessions',"`status` = '0' AND `hidden` = '0'") ){
-							for( $i = 0; $i < sizeof($sessions); $i++ ){
-								echo "<option value='{$sessions[$i]["id"]}'>".direction($sessions[$i]["enTitle"],$sessions[$i]["arTitle"])."</option>";
+						if( $Invoices = selectDB('sessions',"`status` = '0' AND `hidden` = '0'") ){
+							for( $i = 0; $i < sizeof($Invoices); $i++ ){
+								echo "<option value='{$Invoices[$i]["id"]}'>".direction($Invoices[$i]["enTitle"],$Invoices[$i]["arTitle"])."</option>";
 							}
 						}
 					?>
@@ -97,22 +97,80 @@ if( isset($_GET["id"]) && !empty($_GET["id"]) && $student = selectDBNew("student
 		
 		<tbody>
 		<?php 
-		if( $sessions = selectDBNew('studentMore',[0],"`status` = ?","`id` ASC") ){
-			for( $i = 0; $i < sizeof($sessions); $i++ ){
-				$session = selectDB('sessions',"`id` = '{$sessions[$i]["sessionId"]}'");
+		if( $Invoices = selectDBNew('studentMore',[0],"`status` = ?","`id` ASC") ){
+			for( $i = 0; $i < sizeof($Invoices); $i++ ){
+				$session = selectDB('sessions',"`id` = '{$Invoices[$i]["sessionId"]}'");
 				?>
 				<tr>
 				<td><?php echo str_pad(($y = $i + 1), 1, '0', STR_PAD_LEFT) ?></td>
 				<td><?php echo direction($session[0]["enTitle"],$session[0]["arTitle"]) ?></td>
-				<td><?php echo $sessions[$i]["total"] ?></td>
+				<td><?php echo $Invoices[$i]["total"] ?></td>
 				<td class="text-nowrap">
-					<a id="<?php echo $sessions[$i]["id"] ?>" class="mr-25 edit" data-toggle="tooltip" data-original-title="<?php echo direction("Edit","تعديل") ?>"> <i class="fa fa-pencil text-inverse m-r-10"></i>
+					<a id="<?php echo $Invoices[$i]["id"] ?>" class="mr-25 edit" data-toggle="tooltip" data-original-title="<?php echo direction("Edit","تعديل") ?>"> <i class="fa fa-pencil text-inverse m-r-10"></i>
 					</a>
 				<div style="display:none">
-                    <label id="sessionId<?php echo $sessions[$i]["id"]?>"><?php echo $sessions[$i]["sessionId"] ?></label>
-                    <label id="total<?php echo $sessions[$i]["id"]?>"><?php echo $sessions[$i]["total"] ?></label>
-                    <label id="studentId<?php echo $sessions[$i]["id"]?>"><?php echo $sessions[$i]["studentId"] ?></label>
+                    <label id="sessionId<?php echo $Invoices[$i]["id"]?>"><?php echo $Invoices[$i]["sessionId"] ?></label>
+                    <label id="total<?php echo $Invoices[$i]["id"]?>"><?php echo $Invoices[$i]["total"] ?></label>
+                    <label id="studentId<?php echo $Invoices[$i]["id"]?>"><?php echo $Invoices[$i]["studentId"] ?></label>
                 </div>		
+				</td>
+				</tr>
+				<?php
+			}
+		}
+		?>
+		</tbody>
+		
+	</table>
+</div>
+</div>
+</div>
+</div>
+</div>
+</div>
+</div>
+
+<div class="col-sm-12">
+<div class="panel panel-default card-view">
+<div class="panel-heading">
+<div class="pull-left">
+<h6 class="panel-title txt-dark"><?php echo direction("List of Student Invoices","قائمة فواتير الطالب ") . " : " . $student[0]["fullName"] ?></h6>
+</div>
+<div class="clearfix"></div>
+</div>
+<div class="panel-wrapper collapse in">
+<div class="panel-body"> 
+<div class="table-wrap mt-40">
+<div class="table-responsive">
+	<table class="table display responsive product-overview mb-30" id="myTable1">
+		<thead>
+		<tr>
+		<th><?php echo direction("Date","التاريخ") ?></th>
+		<th><?php echo direction("Invoice#","رقم الفاتورة") ?></th>
+		<th><?php echo direction("Price","السعر") ?></th>
+		<th><?php echo direction("Status","الحالة") ?></th>
+		</tr>
+		</thead>
+		
+		<tbody>
+		<?php 
+		if( $Invoices = selectDBNew('invoices',[$_GET["id"]],"`studentId` = ?","`id` DESC") ){
+			for( $i = 0; $i < sizeof($Invoices); $i++ ){
+				$Invocie = selectDB('invoices',"`id` = '{$Invoices[$i]["id"]}'");
+                // status 1 = paid, 2 = cancelled, 0 = pending
+                if( $Invoices[$i]["status"] == 1 ){
+                    $status = direction("Paid","مدفوعة");
+                }elseif( $Invoices[$i]["status"] == 2 ){
+                    $status = direction("Cancelled","ملغية");
+                }else{
+                    $status = direction("Pending","قيد الانتظار");
+                }
+				?>
+				<tr>
+				<td><?php echo $Invoices[$i]["date"] ?></td>
+				<td><a href="?v=InvoiceDetails&id=<?php echo $Invoices[$i]["id"] ?>" target="_blank"><?php echo $Invoices[$i]["id"] ?></td>
+				<td><?php echo $Invoices[$i]["price"] ?></td>
+				<td><?php echo $status ?></td>		
 				</td>
 				</tr>
 				<?php
