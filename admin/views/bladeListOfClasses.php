0c256eba1f1eafc43fb53c0e100637495c6a2e1a
MRxwr <nasserhatab@gmail.com>
2024-08-05 00:13:38 +0300

    Update bladeListOfClasses.php

--- admin/views/bladeListOfClasses.php
+++ admin/views/bladeListOfClasses.php
@@ -162,6 +162,7 @@ if( isset($_POST["sessionId"]) ){
 </div>
 </div>
 <div class="row"><div class="col-md-6"><input type="text" name="note" class="form-control" placeholder="<?php echo direction("Notes","ملاحظات") ?>"></div><div class="col-md-6 p-1"><button class="btn btn-primary"><?php echo direction("Submit","أرسل") ?></button></div></div>
+<div class="clearfix"></div>
 </div>
 </div>
 </div>
