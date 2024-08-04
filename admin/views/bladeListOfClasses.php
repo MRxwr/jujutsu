f6ea5d8a6ec2599a57752268ea33967ad18473ae
MRxwr <nasserhatab@gmail.com>
2024-08-04 22:43:30 +0300

    Update bladeListOfClasses.php

--- admin/views/bladeListOfClasses.php
+++ admin/views/bladeListOfClasses.php
@@ -1,5 +1,6 @@
 <?php 
 if( isset($_POST["sessionId"]) ){
+	unset($_POST["myTable_length"]);
 	if ( $seesion = selectDBNew('attendance',[$userID,$_POST["sessionId"],date("Y-m-d")],"`tainerId` = ? AND `sessionId` = ? AND `date` LIKE '%?%'","") ){
 		if( updateDB('attendance', $_POST, "`id` = '{$seesion[0]["id"]}'") ){
 			header("LOCATION: ?v=ListOfClasses");
@@ -11,7 +12,7 @@ if( isset($_POST["sessionId"]) ){
 		<?php
 		}
 	}else{
-		var_dump($_POST);
+		unset($_POST["myTable_length"]);
 		if( insertDB('attendance', $_POST) ){
 			header("LOCATION: ?v=ListOfClasses");
 		}else{
