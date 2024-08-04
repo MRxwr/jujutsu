4b91cd5073cf0d05bc784558931dae265641e67f
MRxwr <nasserhatab@gmail.com>
2024-08-04 23:55:46 +0300

    Update bladeListOfClasses.php

--- admin/views/bladeListOfClasses.php
+++ admin/views/bladeListOfClasses.php
@@ -93,10 +93,10 @@ if( isset($_POST["sessionId"]) ){
 		<?php 
 		if( isset($_POST["id"]) && !empty($_POST["id"]) && $students = selectDBNew('studentMore',[$_POST["id"]],"`sessionId` = ? AND `total` > 0","") ){
 			if( $attendance = selectDBNew('attendance',[$userID,$_POST["id"],date("Y-m-d")],"`trainerId` = ? AND `sessionId` = ? AND DATE(`date`) = ?","") ){
-				
 				$attendance = json_decode($attendance[0]["attendance"],true);
-				$studentList = json_decode($attendance[0]["studentList"],true);
 				var_dump($attendance);
+				$studentList = json_decode($attendance[0]["studentList"]);
+				
 			}else{
 				$attendance = [];
 				$studentList = [];
