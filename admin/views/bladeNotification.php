35bebd27ca5f754f5a41a73a34bf831d7e5e98e9
MRxwr <nasserhatab@gmail.com>
2024-10-06 11:45:20 +0300

    Update bladeNotification.php

--- admin/views/bladeNotification.php
+++ admin/views/bladeNotification.php
@@ -1,35 +1,10 @@
 <?php 
-if( isset($_GET["hide"]) && !empty($_GET["hide"]) ){
-	if( updateDB('branches',array('hidden'=> '2'),"`id` = '{$_GET["hide"]}'") ){
-		header("LOCATION: ?v=Branches");
-	}
-}
-
-if( isset($_GET["show"]) && !empty($_GET["show"]) ){
-	if( updateDB('branches',array('hidden'=> '1'),"`id` = '{$_GET["show"]}'") ){
-		header("LOCATION: ?v=Branches");
-	}
-}
-
-if( isset($_GET["delId"]) && !empty($_GET["delId"]) ){
-	if( updateDB('branches',array('status'=> '1'),"`id` = '{$_GET["delId"]}'") ){
-		header("LOCATION: ?v=Branches");
-	}
-}
-
-if( isset($_POST["updateRank"]) ){
-	for( $i = 0; $i < sizeof($_POST["rank"]); $i++){
-		updateDB('branches',array("rank"=>$_POST["rank"][$i]),"`id` = '{$_POST["id"][$i]}'");
-	}
-	header("LOCATION: ?v=Branches");
-}
-
 if( isset($_POST["enTitle"]) ){
 	$id = $_POST["update"];
 	unset($_POST["update"]);
 	if ( $id == 0 ){
-		if( insertDB('branches', $_POST) ){
-			header("LOCATION: ?v=Branches");
+		if( insertDB('notifications_list', $_POST) ){
+			header("LOCATION: ?v=NotificationsList");
 		}else{
 		?>
 		<script>
@@ -38,8 +13,8 @@ if( isset($_POST["enTitle"]) ){
 		<?php
 		}
 	}else{
-		if( updateDB('branches', $_POST, "`id` = '{$id}'") ){
-			header("LOCATION: ?v=Branches");
+		if( updateDB('notifications_list', $_POST, "`id` = '{$id}'") ){
+			header("LOCATION: ?v=NotificationsList");
 		}else{
 		?>
 		<script>
@@ -55,7 +30,7 @@ if( isset($_POST["enTitle"]) ){
 <div class="panel panel-default card-view">
 <div class="panel-heading">
 <div class="pull-left">
-	<h6 class="panel-title txt-dark"><?php echo direction("Branch Details","تفاصيل الفرع") ?></h6>
+	<h6 class="panel-title txt-dark"><?php echo direction("Notification Details","تفاصيل الاشعار") ?></h6>
 </div>
 	<div class="clearfix"></div>
 </div>
@@ -75,6 +50,7 @@ if( isset($_POST["enTitle"]) ){
 			
 			<div class="col-md-6" style="margin-top:10px">
 			<input type="submit" class="btn btn-primary" value="<?php echo direction("Submit","أرسل") ?>">
+			<input type="hidden" name="userId" value="<?php echo $userID ?>">
 			<input type="hidden" name="update" value="0">
 			</div>
 		</div>
@@ -114,24 +90,12 @@ if( isset($_POST["enTitle"]) ){
 		
 		<tbody>
 		<?php 
-		if( $branches = selectDB('branches',"`status` = '0' ORDER BY `rank` ASC") ){
+		if( $branches = selectDB('notifications_list',"`status` = '0' ORDER BY `id` ASC") ){
 			for( $i = 0; $i < sizeof($branches); $i++ ){
 				$counter = $i + 1;
-				if ( $branches[$i]["hidden"] == 2 ){
-					$icon = "fa fa-eye";
-					$link = "?v={$_GET["v"]}&show={$branches[$i]["id"]}";
-					$hide = direction("Show","إظهار");
-				}else{
-					$icon = "fa fa-eye-slash";
-					$link = "?v={$_GET["v"]}&hide={$branches[$i]["id"]}";
-					$hide = direction("Hide","إخفاء");
-				}
 				?>
 				<tr>
-				<td>
-				<input name="rank[]" class="form-control" type="number" value="<?php echo $counter ?>">
-				<input name="id[]" class="form-control" type="hidden" value="<?php echo $branches[$i]["id"] ?>">
-				</td>
+				<td><?php echo str_pad($counter,2,"0",STR_PAD_LEFT) ?></td>
 				<td id="enTitle<?php echo $branches[$i]["id"]?>" ><?php echo $branches[$i]["enTitle"] ?></td>
 				<td id="arTitle<?php echo $branches[$i]["id"]?>" ><?php echo $branches[$i]["arTitle"] ?></td>
 				<td class="text-nowrap">
