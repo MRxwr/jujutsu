e5ef46649df394e7f227464765b521efc9438bc3
MRxwr <nasserhatab@gmail.com>
2024-10-07 01:54:06 +0300

    Update payment.php

--- admin/includes/functions/payment.php
+++ admin/includes/functions/payment.php
@@ -409,26 +409,21 @@ function submitUpayment($data){
 function checkUpayment($trackId){
 	GLOBAL $PaymentAPIKey;
 	$curl = curl_init();
-	curl_setopt_array($curl, [
+	curl_setopt_array($curl, array(
 	CURLOPT_URL => "https://sandboxapi.upayments.com/api/v1/get-payment-status/{$trackId}",
 	CURLOPT_RETURNTRANSFER => true,
-	CURLOPT_ENCODING => "",
+	CURLOPT_ENCODING => '',
 	CURLOPT_MAXREDIRS => 10,
-	CURLOPT_TIMEOUT => 30,
+	CURLOPT_TIMEOUT => 0,
+	CURLOPT_FOLLOWLOCATION => true,
 	CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
-	CURLOPT_CUSTOMREQUEST => "GET",
-	CURLOPT_HTTPHEADER => [
-		"Authorization: Bearer {$PaymentAPIKey}",
-		"accept: application/json"
-	],
-	]);
+	CURLOPT_CUSTOMREQUEST => 'GET',
+	CURLOPT_HTTPHEADER => array(
+		"Authorization: {$PaymentAPIKey}"
+	),
+	));
 	$response = curl_exec($curl);
-	$err = curl_error($curl);
 	curl_close($curl);
-	if ($err) {
-		return 0;
-	}else{
-		return $response;
-	}
+	return $response;
 }
 ?>
