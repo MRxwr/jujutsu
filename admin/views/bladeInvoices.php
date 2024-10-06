7aa5ee19fefe04442ad9f6829e1fe55208059441
MRxwr <nasserhatab@gmail.com>
2024-10-07 00:59:43 +0300

    Update bladeInvoices.php

--- admin/views/bladeInvoices.php
+++ admin/views/bladeInvoices.php
@@ -12,9 +12,120 @@ if( isset($_GET["id"]) && !empty($_GET["id"]) ){
     <?php
     }
 }
+
+if( isset($_GET["hide"]) && !empty($_GET["hide"]) ){
+	if( updateDB('invoices',array('hidden'=> '2'),"`id` = '{$_GET["hide"]}'") ){
+		header("LOCATION: ?v=Invoices");
+	}
+}
+
+if( isset($_GET["show"]) && !empty($_GET["show"]) ){
+	if( updateDB('invoices',array('hidden'=> '1'),"`id` = '{$_GET["show"]}'") ){
+		header("LOCATION: ?v=Invoices");
+	}
+}
+
+if( isset($_GET["delId"]) && !empty($_GET["delId"]) ){
+	if( updateDB('invoices',array('status'=> '1'),"`id` = '{$_GET["delId"]}'") ){
+		header("LOCATION: ?v=Invoices");
+	}
+}
+
+if( isset($_POST["updateRank"]) ){
+	for( $i = 0; $i < sizeof($_POST["rank"]); $i++){
+		updateDB('invoices',array("rank"=>$_POST["rank"][$i]),"`id` = '{$_POST["id"][$i]}'");
+	}
+	header("LOCATION: ?v=Invoices");
+}
+
+if( isset($_POST["student"]) ){
+	$id = $_POST["update"];
+	unset($_POST["update"]);
+	if ( $id == 0 ){
+		if( insertDB('invoices', $_POST) ){
+			header("LOCATION: ?v=Invoices");
+		}else{
+		?>
+		<script>
+			alert("Could not process your request, Please try again.");
+		</script>
+		<?php
+		}
+	}else{
+		if( updateDB('invoices', $_POST, "`id` = '{$id}'") ){
+			header("LOCATION: ?v=Invoices");
+		}else{
+		?>
+		<script>
+			alert("Could not process your request, Please try again.");
+		</script>
+		<?php
+		}
+	}
+}
 ?>
 <div class="row">			
+<div class="col-sm-12">
+<div class="panel panel-default card-view">
+<div class="panel-heading">
+<div class="pull-left">
+	<h6 class="panel-title txt-dark"><?php echo direction("Invoice Details","تفاصيل الفاتورة") ?></h6>
+</div>
+	<div class="clearfix"></div>
+</div>
+<div class="panel-wrapper collapse in">
+<div class="panel-body">
+	<form class="" method="POST" action="" enctype="multipart/form-data">
+		<div class="row m-0">
+			<div class="col-md-3">
+			<label><?php echo direction("Studen","المتدرب") ?></label>
+			<select name="student" class="form-control" required>
+				<?php
+				if( $students = selectDB('students',"`status` = '0' ") ){
+					for( $i = 0; $i < sizeof($students); $i++){
+						echo '<option value="'.$student[$i]["id"].'">'.$student[$i]["fullName"].'</option>';
+					}
+				}
+				?>
+			</select>
+			</div>
+			
+			<div class="col-md-3">
+			<label><?php echo direction("Class","الصف") ?></label>
+			<select name="student" class="form-control" required>
+				<?php
+				if( $sessions = selectDB('sessions',"`status` = '0' ") ){
+					for( $i = 0; $i < sizeof($sessions); $i++){
+						echo '<option value="'.$sessions[$i]["id"].'">'.direction($sessions[$i]["enTitle"],$sessions[$i]["arTitle"]).'</option>';
+					}
+				}
+				?>
+			</select>
+			</div>
 
+			<div class="col-md-3">
+			<label><?php echo direction("Mobile","الهاتف") ?></label>
+			<input type="text" name="phone" class="form-control" required>
+			</div>
+
+			<div class="col-md-3">
+			<label><?php echo direction("Price","السعر") ?></label>
+			<input type="text" name="price" class="form-control" required>
+			</div>
+			
+			<div class="col-md-6" style="margin-top:10px">
+			<input type="submit" class="btn btn-primary" value="<?php echo direction("Generate Link","إنشاء رابط") ?>">
+			<input type="hidden" name="update" value="0">
+			</div>
+		</div>
+	</form>
+</div>
+</div>
+</div>
+</div>
+
+
+<div class="row">			
 <div class="col-sm-12">
 <div class="panel panel-default card-view">
 <div class="panel-heading">
