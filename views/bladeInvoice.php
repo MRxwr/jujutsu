e55a87d6a11d38e318dfd45057fbaa7655ce28ca
MRxwr <nasserhatab@gmail.com>
2024-10-08 01:29:30 +0300

    Update bladeInvoice.php

--- views/bladeInvoice.php
+++ views/bladeInvoice.php
@@ -134,9 +134,6 @@ if( isset($_GET["requested_order_id"]) && !empty($_GET["requested_order_id"]) &&
                 <!-- Add your terms and conditions text here -->
                 <p><?php echo $termsAndConditionsText ?></p>
             </div>
-            <div class="modal-footer">
-                <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
-            </div>
         </div>
     </div>
 </div>
