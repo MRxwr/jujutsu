c563450c93fbc9fdb4063bf59be29c922b94c960
MRxwr <nasserhatab@gmail.com>
2024-10-08 00:58:54 +0300

    save

--- template/header.php
+++ template/header.php
@@ -6,7 +6,7 @@
     <title>BASIC JJ - Invoice</title>
 
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap/4.6.2/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap/4.6.2/css/bootstrap.min.css">
     <script src="https://use.fontawesome.com/245c9398b0.js"></script>
 </head>
 
