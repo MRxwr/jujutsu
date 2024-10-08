7186054978bbeb5abd350fd825f743bac40a8425
MRxwr <nasserhatab@gmail.com>
2024-10-08 10:02:09 +0300

    Update notification.php

--- admin/includes/functions/notification.php
+++ admin/includes/functions/notification.php
@@ -281,4 +281,36 @@ function addSeen($users){
 	}, $users, array_keys($users));
 	return $modifiedArray;
 }
+
+function sendUltramsg($data){
+	$params=array(
+		'token' => 'ubuoz8nlunug6bwn',
+		'to' => "{$data["to"]}",
+		'body' => "{$data["msg"]}"
+	);
+	$curl = curl_init();
+	curl_setopt_array($curl, array(
+	  CURLOPT_URL => "https://api.ultramsg.com/instance89028/messages/chat",
+	  CURLOPT_RETURNTRANSFER => true,
+	  CURLOPT_ENCODING => "",
+	  CURLOPT_MAXREDIRS => 10,
+	  CURLOPT_TIMEOUT => 30,
+	  CURLOPT_SSL_VERIFYHOST => 0,
+	  CURLOPT_SSL_VERIFYPEER => 0,
+	  CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+	  CURLOPT_CUSTOMREQUEST => "POST",
+	  CURLOPT_POSTFIELDS => http_build_query($params),
+	  CURLOPT_HTTPHEADER => array(
+	    "content-type: application/x-www-form-urlencoded"
+	  ),
+	));
+	$response = curl_exec($curl);
+	$err = curl_error($curl);
+	curl_close($curl);
+	if ($err) {
+	  echo "cURL Error #:" . $err;
+	} else {
+	  echo $response;
+	}
+}
 ?>
