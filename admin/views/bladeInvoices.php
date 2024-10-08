2c64f037ce6f867ae581ae9b20b22823184e299b
MRxwr <nasserhatab@gmail.com>
2024-10-08 11:54:24 +0300

    Update bladeInvoices.php

--- admin/views/bladeInvoices.php
+++ admin/views/bladeInvoices.php
@@ -200,7 +200,7 @@ if( isset($_POST["studentId"]) ){
 					<?php 
 					if( $Invoices[$i]["status"] == 0 ){
 						$Invoices[$i]["gatewayResponse"] = json_decode($Invoices[$i]["gatewayResponse"],true);
-						$link = "{$settingsWebsite}/?v=Invoice&requested_order_id={$invoices[0]["gatewayId"]}";
+						$link = "{$settingsWebsite}/?v=Invoice&requested_order_id={$Invoices[0]["gatewayId"]}";
 					?>
 					<a href='<?php echo "https://wa.me/{$Invoices[$i]["mobile"]}?text={$notifications[0]["arTitle"]}{$link}" ?>' target="_blank" style="align-content: center;" class="btn btn-info btn-xs"><?php echo direction("Send Link","إرسال الرابط") ?></a>
 					<?php
